3b743f4542210f05ed7430aa974ec81aed646fae
Osama Ali <osama.websecure@gmail.com>
2023-11-12 23:14:28 +0500

    Last Osama

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Nav from './Components/Nav';
-import Footer from './Components/Footer';
-import Mainpage from './Components/Section/MainPage';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Nav from "./Components/Nav";
+import Footer from "./Components/Footer";
+import Mainpage from "./Components/Section/MainPage";
 
-import Home from './Components/Home Page/Home';
-import Service from './Components/Service/Service';
+import Home from "./Components/Home Page/Home";
+import Service from "./Components/Service/Service";
 import Team from "./Components/Team/Team";
 import Project from "./Components/Project/Project";
 import Events from "./Components/Team/Events";
-import Collaboratives from './Components/Team/Collaboratives';
+import Collaboratives from "./Components/Team/Collaboratives";
 import About from ".//Components/About/About";
 import Contact from "./Components/Contact/Contact";
 
-
-
-
-function App() {  
+function App() {
   return (
     <>
-    <BrowserRouter>
- 
+      <BrowserRouter>
+        <Nav />
 
-      <Nav />
-    
         <Routes>
-          <Route  path="/" exact Component={Mainpage} />
-          <Route  path="/home" Component={Home} />
-          <Route  path="/service"  Component={Service} />
-          <Route  path="/team" Component={Team} />
-          <Route  path="/collaborative"  Component={Collaboratives} />
-          <Route  path="/ُevents"  Component={Events} />
-          <Route  path="/project"  Component={Project} />
-          <Route  path="/about" Component={About} />
-          <Route  path="/contact"  Component={Contact} />
+          <Route path="/" exact Component={Mainpage} />
+          <Route path="/home" Component={Home} />
+          <Route path="/service" Component={Service} />
+          <Route path="/team" Component={Team} />
+          <Route path="/collaborative" Component={Collaboratives} />
+          <Route path="/ُevents" Component={Events} />
+          <Route path="/project" Component={Project} />
+          <Route path="/about" Component={About} />
+          <Route path="/contact" Component={Contact} />
         </Routes>
 
-      <Footer />  
-
-    </BrowserRouter>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
 
 export default App;
-
--- src/Components/Section/MainPage.js
+++ src/Components/Section/MainPage.js
@@ -9,15 +9,21 @@ import Clientsec from './Clientsec';
 const Default = () => {
   return (
     <>
+ <div className='container-fluid p-5 m-auto'>
+  <div className='row img-bg pt-5'>
+    <div className='col-12 text-center'>
+      <h2 className='large'>Quds Publisher's</h2>
 
-    <div className="row py-3 m-auto" >
+    </div>
+  </div>
+    <div className="row py-3 mt-5 " >
       {/* Card 1 */}
-      <div className="col-md-4">
+      <div className="col-md-4 col-sm-12">
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">Journals</h5>
             <p className="card-text align-content-center">Will be publish Soon.</p>
-            <Link to= "https://www.qudspub.com" target='_blank'>
+            <Link to= "#" target='_blank'>
           <button className="btn btn-primary">Live Soon</button>
  </Link>
           </div>
@@ -26,12 +32,14 @@ const Default = () => {
 
       {/* Card 2 */}
       <div className="col-md-4">
-        <div className="card">
+        <div className="card ">
           <div className="card-body">
             <h5 className="card-title">Publishing
 House</h5>
             <p className="card-text">Check out serveses for publication in best Q1 and Q2 jornals.</p>
+            <Link to= "https://www.qudspub.com" target='_top'>
             <button className="btn btn-primary" to="">Visit To Site</button>
+            </Link>
           </div>
         </div>
       </div>
@@ -42,10 +50,13 @@ House</h5>
           <div className="card-body">
             <h5 className="card-title">In-silico Servics</h5>
             <p className="card-text">Will be publish Soon.</p>
+            <Link to= "#" target='_blank'>
             <button className="btn btn-primary" to="">Live Soon</button>
+            </Link>
           </div>
         </div>
       </div>
+    </div>
     </div>,
     <Clientsec/>
     <Routes>
